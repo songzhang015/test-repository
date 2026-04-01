57e7859e51fe6b40d195aa33c7577c5ca52cb3ed
Song <songzhang015@gmail.com>
2026-04-01 14:07:02 -0700

    Add ping route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -13,6 +13,10 @@ def health():
 @app.route("/about")
 def about():
     return "About Page!"
+
+@app.route('/ping')
+def ping():
+    return jsonify({"message": "pong"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
